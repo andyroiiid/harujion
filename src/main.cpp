1a8c6f4c24336a1a1adad194dc26067ef5321e76
Andrew Huang <andyroiiid@live.com>
2021-06-13 16:36:14 +0800

    remove script; directly operate on the sol

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,55 @@
+#include <GLFW/glfw3.h>
+#include <glad/gl.h>
+#include <sol/sol.hpp>
+#include <spdlog/spdlog.h>
+
 #include "physfs.h"
-#include "script.h"
 #include "window.h"
+
+void checkLuaResult(const sol::protected_function_result &result) {
+    if (!result.valid()) {
+        sol::error error = result;
+        spdlog::error("lua error: {}", error.what());
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main() {
     auto &physfs = Physfs::getInstance();
     physfs.mount("assets", "/");
 
-    auto &script = Script::getInstance();
-    script.scriptFile("main.lua");
+    sol::state lua;
+    lua.open_libraries(sol::lib::base, sol::lib::package, sol::lib::math);
+    lua.add_package_loader(Physfs::luaPackageLoader);
+
+    sol::table haru = lua.create_table("haru");
+    float clearColor[4] = {0.4f, 0.8f, 1.0f, 1.0f};
+    haru["clearColor"] = std::ref(clearColor);
 
     auto &window = Window::getInstance();
+    auto result = lua.script(physfs.readFile("main.lua"), "main.lua");
+    checkLuaResult(result);
+
+    lua.script("for k, v in pairs(haru) do print(k, v) end");
+    checkLuaResult(result);
+
+    double prevTime = glfwGetTime();
     while (!window.shouldClose()) {
         window.pollEvents();
+
+        double currTime = glfwGetTime();
+        auto deltaTime = static_cast<float>(currTime - prevTime);
+        result = haru["update"](deltaTime);
+        checkLuaResult(result);
+        prevTime = currTime;
+
+        glClearBufferfv(GL_COLOR, 0, clearColor);
+
+        result = haru["draw"]();
+        checkLuaResult(result);
+
         window.swapBuffers();
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
